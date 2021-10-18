745835b648f0a049be2bc6bf5c0297f81a1106a5
DonatellaC <donatella.carta@mailbox.org>
2021-10-18 18:46:35 +0200

    added style

--- src/components/Countdown/Countdown.js
+++ src/components/Countdown/Countdown.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
+import './Countdown.css';
 
 const Countdown = () => {
     const [days, setDays] = useState("");
     const [hours, setHours] = useState("");
     const [minutes, setMinutes] = useState("");
     const [seconds, setSeconds] = useState("");
-    const [time, setTime] = useState(false);
+    const [countDownOver, setCountDownOver] = useState(false);
 
     const timer = () => {
         const countDownDate = new Date("Jan 01, 2022 00:00:00").getTime();
@@ -18,10 +19,20 @@ const Countdown = () => {
 
         // Get time remaining
         const secondsRemaining = timeRemaining / 1000;
-        const daysLeft = Math.floor(secondsRemaining / 3600 / 24);
-        const hoursLeft = Math.floor((secondsRemaining / 3600) % 24);
-        const minutesLeft = Math.floor((secondsRemaining / 60) % 60);
-        const secondsLeft = Math.floor(secondsRemaining % 60);
+        let daysLeft = Math.floor(secondsRemaining / 3600 / 24);
+        let hoursLeft = Math.floor((secondsRemaining / 3600) % 24);
+        let minutesLeft = Math.floor((secondsRemaining / 60) % 60);
+        let secondsLeft = Math.floor(secondsRemaining % 60);
+
+        // Add 0 in front of the time when this is < 10
+        function formatTime(time) {
+            return time < 10 ? (`0${time}`) : time;
+        }
+
+        daysLeft = formatTime(daysLeft)
+        hoursLeft = formatTime(hoursLeft)
+        minutesLeft = formatTime(minutesLeft)
+        secondsLeft = formatTime(secondsLeft)
 
         setDays(daysLeft);
         setHours(hoursLeft);
@@ -33,7 +44,7 @@ const Countdown = () => {
             setHours(0);
             setMinutes(0);
             setSeconds(0);
-            setTime(true);
+            setCountDownOver(true);
         }
     };
 
@@ -43,27 +54,29 @@ const Countdown = () => {
 
     return (
         <>
-            {time ? (
+            {countDownOver ? (
                 <h1>Happy New Year</h1>
             ) : (
-                <section>
+                <section className="container-countdown">
                     <h1>Countdown New Year</h1>
-                    <article>
-                        <p>{days}</p>
-                        <h2>Days</h2>
-                    </article>
-                    <article>
-                        <p>{hours}</p>
-                        <h2>Hours</h2>
-                    </article>
-                    <article>
-                        <p>{minutes}</p>
-                        <h2>Minutes</h2>
-                    </article>
-                    <article>
-                        <p>{seconds}</p>
-                        <h2>Seconds</h2>
-                    </article>
+                    <div className="countdown">
+                        <article>
+                            <p>{days}</p>
+                            <h2>Days</h2>
+                        </article>
+                        <article>
+                            <p>{hours}</p>
+                            <h2>Hours</h2>
+                        </article>
+                        <article>
+                            <p>{minutes}</p>
+                            <h2>Minutes</h2>
+                        </article>
+                        <article>
+                            <p>{seconds}</p>
+                            <h2>Seconds</h2>
+                        </article>
+                    </div>
                 </section>
             )}
         </>
